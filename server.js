ca309944ffd19dee62429bf7759871bc115e5d4c
vcvos52 <vcvos52@mit.edu>
2019-01-02 00:55:15 -0500

    Procfile

--- server.js
+++ server.js
@@ -27,5 +27,25 @@ io.on('connection', function (socket) {
     });
   });
 
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log('listening on port 3000'));
 
-http.listen(3000, () => console.log('listening on port 3000'));
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
